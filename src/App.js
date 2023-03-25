8cc7b73fc68040a22bdc349443c3654469570285
Khai <nghuukhai.ds@gmail.com>
2023-03-25 15:11:20 +0700

    sas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom'
+import Home from "./feature/Home/Home"
+import Room from "./feature/Room/Room"
+import About from "./feature/About/About"
+import Blog from "./feature/Blog/Blog"
+import ContactUs from "./feature/ContactUs/ContactUs"
+import Login from "./feature/Login/Login"
+import Applayout from './feature/Applayout/Applayout';
+import { useContext } from "react"
+import Admin from './feature/Admin/Admin';
+import { ProtectedRoute } from './feature/ProtectedRoot/ProtectedRoot';
+import LoginAdmin from './feature/Admin/LoginAdmin';
+import Booking from './feature/Admin/Booking';
+import Addroom from './feature/Admin/Addroom';
+import { db } from './firebase/config';
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/loginadmin" element={<LoginAdmin />}></Route>
+        <Route path="/" element={<Applayout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/room" element={<Room />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/contactus" element={<ContactUs />}></Route>
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/admin" element={<Admin />}>
+            <Route path="/admin" element={<Booking />}></Route>
+            <Route path="/admin/room" element={<Addroom />}></Route>
+          </Route>
+        </Route>
+
+
+      </Routes>
+
+
+    </div >
   );
 }
 
